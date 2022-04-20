cb4f2848f75f1333514e1a0ecdc578cbf7d32666
Nauja <jeremymorosi@hotmail.fr>
2022-04-20 14:16:30 +0200

    resize method + better controller interface

--- src/index.ts
+++ src/index.ts
@@ -69,6 +69,33 @@ export interface IGameServer extends IFileServer
 }
 
 /**
+ * Interface for player or AI controllers.
+ */
+export interface IController
+{
+    // Unique controller id
+    id: number;
+
+    // If there is a player or AI bound to this controller
+    isBound: boolean;
+
+    // Label to display
+    label: string;
+
+    /**
+     * Send the game state to this controller.
+     * @param {any} state - Game state
+     */
+    sendState(state: any): void
+
+    /**
+     * Get controller inputs.
+     * @returns {any} Inputs
+     */
+    inputs(): any;
+}
+
+/**
  * Interface for your game.
  */
 export interface IGame
@@ -93,57 +120,41 @@ export interface IGame
      * Stop execution of the game.
      */
     stop(): void;
-
-    /**
-     * This function is called on each frame.
-     * 
-     * This is the function you must bind your AI to.
-     * @param {IGame} game - This game instance.
-     */
-    frame(game: IGame): void;
-
-    /**
-     * Read output data from current frame of the game.
-     * 
-     * This allows your AI to analyze the current frame.
-     * @param {string} [key] - Name of output to read.
-     * @param {any} [val=undefined] - Default returned value if output is undefined.
-     * @return {any} Output value for the current frame.
-     */
-    output(key: string, val?: any): any;
-
-    /**
-     * Write input data to current frame of the game.
-     * 
-     * This allows your AI to decide what to do for the next frame.
-     * @param {string} [key] - Name of input to write.
-     * @param {any} [val] - New value for this input.
-     */
-    input(key: string, val: any): void;
 }
 
 /**
  * Player embedding the game on desktop or web.
  */
 export interface IPlayer {
-    // root HTML element attributed to game
-    root: HTMLElement,
-    // Server for game files
-    gameServer: IGameServer,
-    // Called when the game is loaded and ready
-    onReady?: () => void,
-    // Notify the game is loaded and ready
-    ready: () => void,
+    // Root HTML element attributed to the game
+    root: HTMLElement;
+
+    // Server for accessing game files
+    gameServer: IGameServer;
+
     /**
-     * Send game state to all or a single controller.
+     * Allow the game to resize the root HTML element to desired size.
+     * @param {number} width - New width
+     * @param {number} height - New height
+     */
+    resize(width: number, height: number): void;
+
+    /**
+     * Must be called by the game when it is loaded and ready.
+     */
+    ready(): void;
+
+    /**
+     * Send the game state to all or a single controller.
      * @param {any} state - Game state
      * @param {number} controllerId - Controller id
      */
-    sendState: (state: any, controllerId?: number) => void
+    sendState(state: any, controllerId?: number): void
+
     /**
-     * Receive input from all or a single controller.
+     * Get a controller by id.
      * @param {number} controllerId - Controller id
-     * @returns {any} Input state
+     * @returns {IController} Associated controller
      */
-    input: (controllerId?: number) => {[controllerId: number]: any} | any
+    controller(controllerId: number): IController | undefined;
 }
